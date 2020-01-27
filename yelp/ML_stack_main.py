25e866167027a4f230a1737d26bb0c57727d4b56
gavetisyanca <44903895+gavetisyanca@users.noreply.github.com>
2020-01-26 18:40:02 -0800

    New Update
    New Update Comments removal and integration

--- yelp/ML_stack_main.py
+++ yelp/ML_stack_main.py
@@ -36,31 +36,6 @@ from sklearn.metrics import confusion_matrix, classification_report, accuracy_sc
 # from nltk.corpus import stopwords
 stop_words = set(stopwords.words('english')) 
 
-data = []
-with open('english_only_reviews.json', 'r', encoding="utf8") as fl:
-    for line in range(1, 100000):
-        print(f'Reading line {line}')
-        data.append(fl.readline())
-    data = list(map(json.loads, data))
-    df = pd.DataFrame(data)
-df.to_csv('yelp_reviews.csv')
-
- # HERE, AFTER CLEANING DATA WITH clean_d function 
-text_modified = []
-stars_modified = []
-for x in data:
-    for a,y in x.items():
-        if a == 'stars':
-            stars_modified.append(y)
-        if a == 'text':
-            text_modified.append(clean_d(y))
-            
-#we are converting back to lists, so we can create a dataframe out of those lists  
-list_of_stars_text = list(zip(stars_modified, text_modified)) 
-
-# Creating a dataframe with 2 columns [stars and text] 
-df = pd.DataFrame(list_of_stars_text, columns = ['stars', 'text'])
-
 # Checling ,issing values 
 df.isnull().sum()
 
@@ -88,42 +63,6 @@ y = df['stars']
 # Splitting Data into Training and testing  
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=50)
  
-# # COUNT VECTORIZER, TFIDF TRANSFORMER and FITTING THE MODEL SEPARATELY! in several steps #
-# from sklearn.feature_extraction.text import CountVectorizer
-# count_vector = CountVectorizer() # creating an object from the Class CountVectorizer 
-
-# FIT VECTORIZER TO THE DATA (build a vocab, count the number of words...)
-# count_vector.fit(X_train)
-
-# # Transform the original text message ---> VECTOR
-# X_train_counts = count_vector.transform(X_train)
-
-# fit and transform in one step - 1 line (more efficient)
-# X_train_counts = count_vector.fit_transform(X_train)
-
-# NOW WE WANT TO TRANSFORM COUNTS TO FREQUENCIES WITH TFIDF 
-# SO BASICALLY WE ARE FIGURING OUT WHICH WORDS ARE MORE IMPORTANT THAN OTHERS 
-# GIVING WORDS THAT ARE MORE IMPORTANT MORE WEIGHTS 
-# SO WHAT WE ARE GOING TO DO IS TO PASS CountVectorization to TFIDF Transformer
-# from sklearn.feature_extraction.text import TfidfTransformer
-# tfidf_transformer = TfidfTransformer()
-# X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-
-# SO there is better way to do count vecorization and tfidf with 1 shot ! (TfidfVectorizer class)
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# vectorizer = TfidfVectorizer()
-# X_train_Tfidf = vectorizer.fit_transform(X_train)
-
-# NOW WE CAN TRAIN CLASSIFIER 
-# from sklearn.svm import LinearSVC
-# classifier = LinearSVC() # I was experimenting with different models 
-
-# classifier.fit(X_train_Tfidf, y_train) - trained model 
-
-# ONLY training set got vecorized (turned into vocabulary) so we need to turn 
-# test set to vocabulary as well. However, better alternative is to use Pipeline Class , which 
-# can perform both vectorization and classification with one shot !!! 
-
 stop_words = set(stopwords.words('english'))
 text_classifier = Pipeline([
                 ('tfidf', TfidfVectorizer(stop_words=stop_words)),

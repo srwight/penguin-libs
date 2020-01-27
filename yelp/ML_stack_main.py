5e63fe3878686e4142ca722ed3ecdf4e2e4e171d
Desmond <des_e_mond@hotmail.com>
2020-01-27 16:23:07 -0500

    Update ML_stack_main.py

--- yelp/ML_stack_main.py
+++ yelp/ML_stack_main.py
@@ -18,64 +18,14 @@ from preprocessing import preprocessing
 from demoticon import demotify
 from cleaning_data import clean_d
 from preprocess import preprocess
-
-
-
-
 ### Import your sklearn model ###
 # Example: from sklearn.linear_model import SGDClassifier
 from sklearn.svm import LinearSVC
-##################################################### GEORGE A #############################################################################
-# Parameters [ instead of solver = 'sag', used solver = 'liblinear'] - based on sklearn documentations liblinear better for smaller datasets
-# 'sag' for larger ones 
-# 
-# Importing all dependencies and libraries here some of them are imported above 
-from sklearn.linear_model import LogisticRegression
+
 from sklearn.multiclass import OneVsRestClassifier
-# from sklearn.svm import LinearSVC
 # from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline # Feeding into the pipelne so it does Counte Vectorizer, TF Transformer TFIFD 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from cleaning_data import clean_d # Custom Made Reg Expressions Text Cleaning Took 
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
-# from nltk.corpus import stopwords
-stop_words = set(stopwords.words('english')) 
 
-# Checling ,issing values 
-df.isnull().sum()
 
-# WE GET 5 unique values
-df['stars'].unique()
-
-# counts how many of each individual label is in the dataset (column stars)
-df['stars'].value_counts()
-
- # Replacing 2 with 1 and 4 with 5 (labels number of stars)
-df['stars'] = df['stars'].replace({2: 1, 4: 5})
-
-# Replacing 1,3,5 with -1, 0 and 1 SO would reflect Softmax probabilities representations function 
-df['stars'] = df['stars'].replace({1:-1, 3:0, 5:1})
-
-# counts number of unique labels in the stars columns 
-df['stars'].unique()
-
-# Assigning 'text' column to X
-X = df['text']
-
-# Assigning 'stars' values to be y 
-y = df['stars']
-
-# Splitting Data into Training and testing  
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=50)
- 
-stop_words = set(stopwords.words('english'))
-text_classifier = Pipeline([
-                ('tfidf', TfidfVectorizer(stop_words=stop_words)),
-                ('clf', OneVsRestClassifier(LogisticRegression(solver='sag')), # sag - stochastic average gradeint 
-            ])
-# text_classifier.fit(X_train, y_train) - training data (its saved in george_model_joblib
-# Prediction using predict method 
-predictions = text_classifier.predict(X_test)
   
 # confusion_matrix(y_test, predictions) # confusion metrics 
 # classification_report(y_test, predictions) # report of precision, recall, f1-score 
@@ -90,16 +40,21 @@ df = pd.read_csv(path + r'\english100k.csv')
 
 ### Vectorizer ###
 ''' Call the transform method before training or making predictions. '''
-vectorizer = TfidfVectorizer(preprocessor = preprocessing)
+vectorizer = TfidfVectorizer(preprocessor = preprocessing, min_df=2)
 vectorizer = vectorizer.fit(df.text)
 
 ### Place your model below with comments and parameters ###
+
 # This model's default hyperparameters were already optimal for our data.
 lsvc = LinearSVC()
 
+##################################################### GEORGE A #############################################################################
+# Parameters [ instead of solver = 'sag', used solver = 'liblinear'] - based on sklearn documentations liblinear better for smaller datasets
+# 'sag' for larger ones 
+text_classifier = ('clf', OneVsRestClassifier(LogisticRegression(solver='sag')) # sag - stochastic average gradeint 
 
 
-models = [lsvc] # Add each model's variable name to the list.
+models = [lsvc, text_classifier] # Add each model's variable name to the list.
 
 
 ### Train-test split, vectorizing before making predictions ###
